2ec66032a86e57b68bf6b15f229f176c42e06cea
pisey <pisey@marinax.biz>
2018-02-16 17:33:33 +0700

    - Make answer button prefabs.

---
+++ Project/Assets/Scripts/AnswerButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour
+{
+	public Text answerText;
+	public AnswerData answerData;
+	public void Setup(AnswerData data)
+	{
+		answerData = data;
+		answerText.text = answerData.answerText;
+	}
+}
